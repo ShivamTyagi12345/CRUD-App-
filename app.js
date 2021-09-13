ec16c653372e3c9e4b05b362d8b8f5568fa1b8d2
ShivamTyagi <shivamt.ug19.ec@nitp.ac.in>
2021-09-13 19:32:23 +0530

    final

--- app.js
+++ app.js
@@ -8,3 +8,11 @@ const con = mongoose.connection
 con.on('open', () => {
     console.log('connected...')
 })
+app.use(express.json())
+
+const alienRouter = require('./routes/aliens')
+app.use('/aliens',alienRouter)
+
+app.listen(9000, () => {
+    console.log('Server started')
+})
